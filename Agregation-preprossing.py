ccd3c683a0a79cae3e7424312a507effbafacf97
Leonildo <leonildo_mang@hotmail.com>
2022-10-18 16:53:23 -0500

    adding gitignore

--- Agregation-preprossing.py
+++ Agregation-preprossing.py
@@ -1,5 +1,7 @@
+import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from datetime import datetime
 from math import sin, cos, sqrt, atan2, radians
 
@@ -8,17 +10,28 @@ class Agregation:
 
 	def __init__(self, dataset):
 		self.dataset = dataset
-		#removendo colunas indesejaveis
+
+	def initialize(self):
+		#removing undesired columns
 		self.dataset = self.dataset.drop(['Source','Site ID','UNITS','POC','STATE','STATE_CODE','COUNTY_CODE','COUNTY','Daily Max 8-hour Ozone Concentration','Site Name','DAILY_OBS_COUNT','PERCENT_COMPLETE','AQS_PARAMETER_CODE','AQS_PARAMETER_DESC','CBSA_CODE','CBSA_NAME'],axis=1)
-		#filtrando pelo ano desejado
-		# self.dataset = self.dataset[self.dataset['DateLocal'].str.contains("2011")]
-		#dataset = dataset.replace(0, np.nan) #substituindo 0 com NaN
-		self.dataset = self.dataset.dropna() # eliminando valores Nulos
-		#grouped = dataset.groupby(["Latitude","Longitude"]).mean()#count()[['DateLocal']]  # testendo a funcao contar
-		# grouped.to_csv('coletas-poluentes.csv')
-		self.dataset = self.dataset.reset_index()
+		self.dataset = self.dataset.dropna() 
+		# Convert the date to datetime64
+		self.dataset['Date'] = pd.to_datetime(self.dataset['Date'], format='%m/%d/%Y')
+		self.dataset.index = self.dataset['Date'] # to work the next line
+		self.dataset = self.dataset.groupby(pd.Grouper(freq='M')).mean() #grouping data by month
 		print(self.dataset.head())
 		print(self.dataset.tail())
+
+	def plotTimeSeries(self, graphName):
+		plt.rc('font', size=10)
+		plt.plot(self.dataset.index,self.dataset['DAILY_AQI_VALUE'],label="Ozone",color="red") # Plota os dados
+		plt.title('Ozone Polution')
+		plt.xlabel("Date")
+		plt.ylabel("O3")
+		plt.ylim()
+		#plt.show()
+		plt.savefig("./"+graphName)
+		plt.clf()
 
 	#salve the dataframe into a csv
 	def saveData(self, file_name):
@@ -26,9 +39,12 @@ class Agregation:
 
 if __name__ == "__main__":
 	
-	print('Iniciando pre processamento 1')
-	dataSet = pd.read_csv("/home/leonildo/Downloads/ad_viz_plotval_data.csv") #O3
-	print('Agrupando')
-	a = Agregation(dataSet)
-	
-	print('identificando dados nos poligonos')
+	print('Iniciando pre processamento!')
+	path = "/home/leonildo/Downloads/california-data-ozone"
+	dir_list = os.listdir(path)
+	for file in dir_list:
+		readPath = path + '/' + file
+		dataSet = pd.read_csv(readPath) #O3
+		a = Agregation(dataSet)
+		a.initialize()
+		a.plotTimeSeries(file.replace('.csv',''))
